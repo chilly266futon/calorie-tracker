6b9d69b8ec2f1f4e56811136f347276cb592991e
Kirill <kirya.rog@yandex.ru>
2024-04-13 18:15:45 +0300

    added DBistance func to connection.go

--- server/routes/connection.go
+++ server/routes/connection.go
@@ -1,5 +1,29 @@
-package main
+package routes
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
 
+func DBistance() *mongo.Client {
+	MongoDb := "mongo://localhost:27017/caloriesdb"
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
 }
